95c0dd78db765619a0aafc4edda2d9be3a1afd68
Harry Smith <harryawsmith@gmail.com>
2022-07-23 23:43:21 +1000

    So we've made a moderate amount of progress here. Event listeners are really confusing me. I struggled for a good while with this not functioning whatsoever. I managed to get the eventlistener to print the id of the buttons when clicked, but couldn't figure out how to return that result to the playerSelection so it could be used in the playRound function. Now, I've declared the playerSelection variable outside of the function, defined the inputs of the playRound function, and then set the value of playerSelection in the event listener and called the playRound function. This works, but always returns a tie. Minor progress, but who knows where it's messed up now

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
-//This is the external JS file for the Rock Paper Scissors project. 
-//Bit daunted by this one, but lets give it a crack. 
-//The aim here is to play Rock Paper Scissors (RPS) against the computer. 
-
-
-//PSEUDOCODE
-
-//take input of string from user
-//convert from string to integer
-//3 = rock
-//2 = paper
-//1 = scissors
-//create function for computer to randomly return either rock, paper or scissors = getComputerChoice
-//create function that takes two parameters (playerSelection and getComputerChoice) = playRound
-//return a string that declares the winner of the round
-//create function to play 5 round game that keeps score and reports winner or loser at the end = game
-
-
-
-
-function playerInput() {
-    let getPlayerChoice = prompt("Please enter rock, paper or scissors:").toLowerCase();
-    if (getPlayerChoice === "rock") {
-        return 3;
-    } else if (getPlayerChoice === "paper") {
-        return 2;
-    } else if (getPlayerChoice === "scissors") {
-        return 1;
-    }
-}
-
-//here I'm taking the players input, converting it to all lowercase, then converting the answer to an integer.
-
 
 function getComputerChoice() {
     let theRandomNumber = Math.floor(Math.random() * 3) +1;
         return theRandomNumber;
     }
 
-//here I'm generating a random number between 1 and 3 that will be the computers selection.
+const computerSelection = getComputerChoice();
+let playerSelection = 0;
 
-    let userScore = 0;
-    let computerScore = 0;
 
-//declaring two variables to track the scores of the user and computer, setting their initial value to 0
-
-function playRound() {
-    const computerSelection = getComputerChoice();
-    const playerSelection = playerInput();
+function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         return "A tie!"       
@@ -71,32 +33,63 @@ function playRound() {
     }
  }
 
- //running the round by comparing the user selection to the computer selection. Based on the result, the score for either side will tick up by 1 (or none if a tie)
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+        playerSelection = button.id;
+        playRound(playerSelection, computerSelection)
+        console.log(playRound())
+    })
+
+})
 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-     console.log(playRound());
-     console.log("Your score = " + userScore);
-     console.log("Computer's score = " + computerScore);
-    }
-}
+
+    // let userScore = 0;
+    // let computerScore = 0;
+
+
+
+
+//ALL OF THE BELOW HAS BEEN COMMENTED OUT TEMPORARILY
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//      console.log(playRound());
+//      console.log("Your score = " + userScore);
+//      console.log("Computer's score = " + computerScore);
+//     }
+// }
 
 //creating a loop to run the game for 5 rounds and print the score at the end of each round.
 
-function finalResult () {
-    if (userScore > computerScore) {
-        return "The final score is " + userScore + " to " + computerScore + " . You win!";
-    } else if (computerScore > userScore) {
-        return "The final score is " + computerScore + " to " + userScore + " . You lose :(";
-    } else if (computerScore === userScore) {
-        return "The final score is " + userScore + " to " + computerScore + " . It's a tie! Refresh the page to play again.";
-    }
-}
+// function finalResult () {
+//     if (userScore > computerScore) {
+//         return "The final score is " + userScore + " to " + computerScore + " . You win!";
+//     } else if (computerScore > userScore) {
+//         return "The final score is " + computerScore + " to " + userScore + " . You lose :(";
+//     } else if (computerScore === userScore) {
+//         return "The final score is " + userScore + " to " + computerScore + " . It's a tie! Refresh the page to play again.";
+//     }
+// }
 
 //printing the final result and declaring a winner, loser, or a tie.
 
-console.log (game())
-console.log (finalResult())
+// console.log (game())
+// console.log (finalResult())
 
 //console.logs to make the game and final result functions execute and print to the browser console.
+
+
+// TO DO:
+//1. create 3 buttons in the html for the selections
+//2. add event listener to the buttons that call the playRound function with the correct playerSelection every time the button is clicked
+//3. add a div for displaying results and change all of the console.logs into DOM methods
+//4. display the running score and announce a winner of the game once one player reaches 5 points
+//5. once finished, commit changes to the new branch
+
+
+
