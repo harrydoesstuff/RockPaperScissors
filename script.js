7dc7007b48b70b6c39aa9430ecf3746b1049c354
Harry Smith <harryawsmith@gmail.com>
2022-07-26 14:02:04 +1000

    shifted all of the code from newScript.js to this file, also removed the newScript.js file as it's no longer needed

--- script.js
+++ script.js
@@ -1,113 +1,119 @@
+//ok lets start out fresh 
+
+
+//scissors = 1
+//paper = 2
+//rock = 3
+let playerSelection; 
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const roundResult = document.querySelector(".roundResult");
+const computerRoundResult = document.querySelector(".computerScore");
+const playerRoundResult = document.querySelector(".playerScore");
+const endGameResult = document.querySelector(".endGameResult");
+let computerScore = 0
+let playerScore = 0 
+
+
+rock.addEventListener("click", () => {
+    playRound(3);
+})
+
+paper.addEventListener("click", () => {
+    playRound(2);
+})
+
+scissors.addEventListener("click", () => {
+    playRound(1);
+})
 
 function getComputerChoice() {
     let theRandomNumber = Math.floor(Math.random() * 3) +1;
-        return theRandomNumber;
+    return theRandomNumber
     }
 
-const computerSelection = getComputerChoice();
-let playerSelection = 0;
+function playRound (playerSelection, computerSelection) {
 
+    computerSelection = getComputerChoice();
+  
+    
+    if (playerSelection === 3 && computerSelection === 1) {
+       
+        roundResult.textContent = "You win! You chose rock and the computer chose scissors";
+        playerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
+        playerWins();
 
-function playRound(playerSelection, computerSelection) {
+    } else if (playerSelection === 2 && computerSelection === 3) {
+       
+        roundResult.textContent = "You win! You chose paper and the computer chose rock";
+        playerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
+        playerWins();
 
-    if (playerSelection === computerSelection) {
-        return "A tie!"       
-    } else if (playerSelection === 2 && computerSelection === 3){
-        userScore++;
-        return "You win this round! paper beats rock";
     } else if (playerSelection === 1 && computerSelection === 2) {
-        userScore++;
-        return "You win this round! scissors beats paper";
-    } else if  (playerSelection === 3 && computerSelection === 1) {
-        userScore++;
-        return "You win this round! rock beats scissors";
+       
+        roundResult.textContent = "You win! You chose scissors and the computer chose paper";
+        playerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
+        playerWins();
+
     } else if (playerSelection === 3 && computerSelection === 2) {
-        computerScore++;
-        return "You lose! paper beats rock";
+        
+        roundResult.textContent = "You lose :( you chose rock and the computer chose paper";
+        computerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
+        computerWins();
+
     } else if (playerSelection === 2 && computerSelection === 1) {
-        computerScore++;
-        return "You lose! scissors beats paper";
+       
+        roundResult.textContent = "You lose :( you chose paper and the computer chose scissors";
+        computerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
+        computerWins();
+
     } else if (playerSelection === 1 && computerSelection === 3) {
-        computerScore++;
-        return "You lose! rock beats scissors";
+        
+        roundResult.textContent = "You lose :( you chose scissors and the computer chose rock";
+        computerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
+        computerWins();
+
+    } else if (playerSelection === 1 && computerSelection === 1) {
+        roundResult.textContent = "A tie! You both chose scissors"
+    } else if (playerSelection === 2 && computerSelection === 2) {
+        roundResult.textContent = "A tie! You both chose paper"
+    } else if (playerSelection === 3 && computerSelection === 3) {
+        roundResult.textContent = "A tie! You both chose rock"
     }
- }
+}
 
+function playerWins() {
+   
+    if (playerScore === 5) {
+        endGameResult.textContent = "You reached 5, you win. Reload the page to play again."
+        document.querySelector("#rock").disabled = true;
+        document.querySelector("#paper").disabled = true;
+        document.querySelector("#scissors").disabled = true;
+    }
+}
 
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button => button.addEventListener('click', getPlayerChoice)))
-
-function getPlayerChoice(e) {
-    if (e.target = "button#1") {
-        playerSelection = 1;
-    } else if (e.target = "button#2") {
-        playerSelection = 2;
-    } else if (e.target = "button#3") {
-        playerSelection = 3;
+function computerWins() {
+ 
+    if (computerScore === 5) {
+        endGameResult.textContent = "Computer reached 5, you lose. Reload the page to play again."
+        document.querySelector("#rock").disabled = true;
+        document.querySelector("#paper").disabled = true;
+        document.querySelector("#scissors").disabled = true;
     }
 }
 
 
-
-
-    // let userScore = 0;
-    // let computerScore = 0;
-
-
-
-
-//ALL OF THE BELOW HAS BEEN COMMENTED OUT TEMPORARILY
-
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//      console.log(playRound());
-//      console.log("Your score = " + userScore);
-//      console.log("Computer's score = " + computerScore);
-//     }
-// }
-
-//creating a loop to run the game for 5 rounds and print the score at the end of each round.
-
-// function finalResult () {
-//     if (userScore > computerScore) {
-//         return "The final score is " + userScore + " to " + computerScore + " . You win!";
-//     } else if (computerScore > userScore) {
-//         return "The final score is " + computerScore + " to " + userScore + " . You lose :(";
-//     } else if (computerScore === userScore) {
-//         return "The final score is " + userScore + " to " + computerScore + " . It's a tie! Refresh the page to play again.";
-//     }
-// }
-
-//printing the final result and declaring a winner, loser, or a tie.
-
-// console.log (game())
-// console.log (finalResult())
-
-//console.logs to make the game and final result functions execute and print to the browser console.
-
-
-// TO DO:
-//1. create 3 buttons in the html for the selections
-//2. add event listener to the buttons that call the playRound function with the correct playerSelection every time the button is clicked
-//3. add a div for displaying results and change all of the console.logs into DOM methods
-//4. display the running score and announce a winner of the game once one player reaches 5 points
-//5. once finished, commit changes to the new branch
-
-
-
-// const scissors = document.getElementById("1");
-// const paper = document.getElementById("2");
-// const rock = document.getElementById("3");
-
-// scissors.addEventListener("click", function() {
-//     playRound(1, computerSelection);
-// })
-
-// paper.addEventListener("click", function() {
-//     playRound(2, computerSelection);
-// })
-
-// rock.addEventListener("click", function() {
-//     playRound(3, computerSelection);
-// })
