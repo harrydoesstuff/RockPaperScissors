53ed72b39cb17047d7eedf53015d5a5c774993e7
Harry Smith <harryawsmith@gmail.com>
2022-07-26 13:59:18 +1000

    no changes here

--- newScript.js
+++ newScript.js
@@ -40,47 +40,64 @@ function playRound (playerSelection, computerSelection) {
   
     
     if (playerSelection === 3 && computerSelection === 1) {
-        // roundResult.textContent = "player wins";
-        // playerScore++;
+       
+        roundResult.textContent = "You win! You chose rock and the computer chose scissors";
+        playerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
         playerWins();
-        // updateResults();
+
     } else if (playerSelection === 2 && computerSelection === 3) {
-        // roundResult.textContent = "player wins";
-        // playerScore++;
+       
+        roundResult.textContent = "You win! You chose paper and the computer chose rock";
+        playerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
         playerWins();
-        // updateResults();
+
     } else if (playerSelection === 1 && computerSelection === 2) {
-        // roundResult.textContent = "player wins";
-        // playerScore++;
+       
+        roundResult.textContent = "You win! You chose scissors and the computer chose paper";
+        playerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
         playerWins();
-        // updateResults();
+
     } else if (playerSelection === 3 && computerSelection === 2) {
-        // roundResult.textContent = "computer wins";
-        // computerScore++;
+        
+        roundResult.textContent = "You lose :( you chose rock and the computer chose paper";
+        computerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
         computerWins();
-        // updateResults();
+
     } else if (playerSelection === 2 && computerSelection === 1) {
-        // roundResult.textContent = "computer wins";
-        // computerScore++;
+       
+        roundResult.textContent = "You lose :( you chose paper and the computer chose scissors";
+        computerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
         computerWins();
-        // updateResults();
+
     } else if (playerSelection === 1 && computerSelection === 3) {
-        // roundResult.textContent = "computer wins";
-        // computerScore++;
+        
+        roundResult.textContent = "You lose :( you chose scissors and the computer chose rock";
+        computerScore++; 
+        computerRoundResult.innerHTML = computerScore;
+        playerRoundResult.innerHTML = playerScore;
         computerWins();
-        // updateResults();
-    } else if (playerSelection === computerSelection) {
-        tie();
-        // updateResults();
+
+    } else if (playerSelection === 1 && computerSelection === 1) {
+        roundResult.textContent = "A tie! You both chose scissors"
+    } else if (playerSelection === 2 && computerSelection === 2) {
+        roundResult.textContent = "A tie! You both chose paper"
+    } else if (playerSelection === 3 && computerSelection === 3) {
+        roundResult.textContent = "A tie! You both chose rock"
     }
-    
 }
 
 function playerWins() {
-    roundResult.textContent = "You win!"
-    playerScore++; 
-    computerRoundResult.innerHTML = computerScore;
-    playerRoundResult.innerHTML = playerScore;
+   
     if (playerScore === 5) {
         endGameResult.textContent = "You reached 5, you win. Reload the page to play again."
         document.querySelector("#rock").disabled = true;
@@ -90,10 +107,7 @@ function playerWins() {
 }
 
 function computerWins() {
-    roundResult.textContent = "You lose :("
-    computerScore++; 
-    computerRoundResult.innerHTML = computerScore;
-    playerRoundResult.innerHTML = playerScore;
+ 
     if (computerScore === 5) {
         endGameResult.textContent = "Computer reached 5, you lose. Reload the page to play again."
         document.querySelector("#rock").disabled = true;
@@ -102,39 +116,4 @@ function computerWins() {
     }
 }
 
-function tie() {
-    roundResult.textContent = "Tie!" 
-}
 
-function reloadPage() {
-    window.location.reload();
-}
-
-// function updateResults() {
-//     if (computerScore != 5 || playerScore != 5){
-//     computerRoundResult.innerHTML = computerScore;
-//     playerRoundResult.innerHTML = playerScore;
-//     } else if (computerScore === 5 || playerScore === 5){
-//     endGameResult.textContent = "Game Over! The final score is Computer: ", "${computerScore}", " Player: ", "${playerScore}";
-//     }
-// }
-
-
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//      console.log(playRound());
-//      console.log("Your score = " + userScore);
-//      console.log("Computer's score = " + computerScore);
-//     }
-// }
-
-
-// function finalResult () {
-//     if (userScore > computerScore) {
-//         return "The final score is " + userScore + " to " + computerScore + " . You win!";
-//     } else if (computerScore > userScore) {
-//         return "The final score is " + computerScore + " to " + userScore + " . You lose :(";
-//     } else if (computerScore === userScore) {
-//         return "The final score is " + userScore + " to " + computerScore + " . It's a tie! Refresh the page to play again.";
-//     }
-// }
